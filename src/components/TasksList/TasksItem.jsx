7465b6878e9daa84dcd21beed6026078d9945793
kais-blkc <nagliyavarec@gmail.com>
2023-06-13 20:39:53 +0300

    Added ability to change task

--- src/components/TasksList/TasksItem.jsx
+++ src/components/TasksList/TasksItem.jsx
@@ -3,8 +3,13 @@ import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 export default function TasksItem({ item, checkTask, removeTask }) {
+  const [taskVal, setTaskVal] = useState(item.task);
+  const [editable, setEditable] = useState(false);
+
   function checkHandler(id) {
     checkTask(id);
   }
@@ -14,42 +19,63 @@ export default function TasksItem({ item, checkTask, removeTask }) {
   }
 
   function editHandler(id) {
-    console.log(id);
+    setEditable(!editable);
   }
 
-  const checkBtn = item.check ? (
-    <button className="tasks-list__item-checked">
-      <FontAwesomeIcon
-        icon={faCircleCheck}
-        onClick={() => checkHandler(item.id)}
-      />
-    </button>
-  ) : (
-    <button
-      className="tasks-list__item-check"
-      onClick={() => checkHandler(item.id)}
-    >
-      <FontAwesomeIcon icon={faCircle} />
-    </button>
-  );
+  function taskChangeHandler(e) {
+    setTaskVal(e.target.value);
+  }
+
+  function taskKeyDownHandler(e) {
+    if (e.key === 'Enter') setEditable(false);
+  }
+
+  let taskListItemClasses = item.check ? ' task-done' : '';
 
   return (
-    <div className="tasks-list__item group">
-      {checkBtn}
-      <div className="tasks-list__item-text">{item.task}</div>
+    <div className={'tasks-list__item ' + taskListItemClasses}>
+      <div className="tasks-list__item-left">
+        <button
+          className="tasks-list__item-checked"
+          onClick={() => checkHandler(item.id)}
+        >
+          {item.check ? (
+            <FontAwesomeIcon icon={faCircleCheck} />
+          ) : (
+            <FontAwesomeIcon icon={faCircle} />
+          )}
+        </button>
 
-      <button
-        className="tasks-list__item-edit group-hover:opacity-100"
-        onClick={() => editHandler(item.id)}
-      >
-        <FontAwesomeIcon icon={faPenToSquare} />
-      </button>
-      <button
-        className="tasks-list__item-delete"
-        onClick={() => removeHandler(item.id)}
-      >
-        <FontAwesomeIcon icon={faCircleXmark} />
-      </button>
+        <div className="tasks-list__item-text">
+          <input
+            type="text"
+            value={taskVal}
+            readOnly={!editable}
+            onChange={taskChangeHandler}
+            onKeyDown={taskKeyDownHandler}
+          />
+        </div>
+      </div>
+
+      <div className="tasks-list__item-right">
+        <button
+          className="tasks-list__item-edit"
+          onClick={() => editHandler(item.id)}
+        >
+          {editable ? (
+            <FontAwesomeIcon icon={faCheck} />
+          ) : (
+            <FontAwesomeIcon icon={faPenToSquare} />
+          )}
+        </button>
+
+        <button
+          className="tasks-list__item-delete"
+          onClick={() => removeHandler(item.id)}
+        >
+          <FontAwesomeIcon icon={faCircleXmark} />
+        </button>
+      </div>
     </div>
   );
 }
